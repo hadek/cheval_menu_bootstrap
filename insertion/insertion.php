fd554b54b09ba9973d72bdf4e9d0f965422e6154
hadek <hadekfadel@gmail.com>
2016-09-25 17:52:35 +0200

    ajout d'une fonctionnalité dans le fichier insertion

--- insertion/insertion.php
+++ insertion/insertion.php
@@ -4,22 +4,29 @@
 // affichage des données de l'armoire print_r($_POST);
 //affichage de l'armoire print_r($_FILES);
 //tjrs :on commence par inclure le fichier connexion
-include('connexion.php');
+include('../connexion.php');
 // vider les données de l'armoire $_POST
 //Attention à la casse des variables dans le formulaire !!!
-$nom = $_POST['nom'];
+$nom_cheval = $_POST['nom'];
 $race = $_POST['race'];
-//traitement de l'image 
-echo $_FILES['image']['tmp_name'];
+/**** Etape 1: l'upload du fichier image ****/
+//si le fichier n'est pas trop grand (risque de saturation de la E.H)
 if($_FILES['image']['size'] < 1048576) {
-	 $nom = md5(uniqid(rand(), true));
- 	
-	move_uploaded_file($_FILES['image']['tmp_name'],'images/'.$nom.'.png');
+	// génération d'un nom au hasard du fichier image 
+	 $nom_image = md5(uniqid(rand(), true)).'.png';
+ 	// charger l'image avec le nouveau nom généré et le stocker dans image 
+	move_uploaded_file($_FILES['image']['tmp_name'],'../images/'.$nom_image);
 }else {
-	
  	echo 'le fichier est trop grand';
-
-
 }	
+/******Etape 2: Insertion des renseignements dans la table ***/
+
+//la requête
+$req= " INSERT INTO `cheval` ( `nom`, `race`, `image`) VALUES ( '$nom_cheval','$race', '$nom_image')";
+$resultat = $bdd-> exec($req);
+//message en cas d'erreur
+if($resultat)   echo " le cheval à bien été ajouté" ;
+else echo " désolé, il y avait une erreur de soumission "; 
+
 
 ?>
